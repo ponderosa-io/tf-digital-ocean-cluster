02e14691c42b1f74dd4aea2a9b5439b60820afef
Alejandro Saucedo <axsauze@gmail.com>
2020-01-24 18:11:38 +0000

    Updating variable values for 0.12 compatibility

--- digital-ocean-cluster.tf
+++ digital-ocean-cluster.tf
@@ -1,15 +1,15 @@
-variable "do_token" {}
-variable "do_cluster_name" {}
+variable do_token {}
+variable do_cluster_name {}
 
 # Get a Digital Ocean token from your Digital Ocean account
 #   See: https://www.digitalocean.com/docs/api/create-personal-access-token/
 # Set TF_VAR_do_token to use your Digital Ocean token automatically
 provider "digitalocean" {
-  token = "${var.do_token}"
+  token = var.do_token
 }
 
 resource "digitalocean_kubernetes_cluster" "my_digital_ocean_cluster" {
-  name    = "${var.do_cluster_name}"
+  name    = var.do_cluster_name
   region  = "nyc1"
   version = "1.13.1-do.2"
 
@@ -21,5 +21,5 @@ resource "digitalocean_kubernetes_cluster" "my_digital_ocean_cluster" {
 }
 
 output "cluster-id" {
-  value = "${digitalocean_kubernetes_cluster.my_digital_ocean_cluster.id}"
+  value = digitalocean_kubernetes_cluster.my_digital_ocean_cluster.id
 }
